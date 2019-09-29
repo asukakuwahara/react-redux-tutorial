c8c0a4edebf4bea129865a47ca51b615c3b07034
asucarlos <kuwahara.mee@gmail.com>
2019-09-29 16:29:42 -0400

    add reach router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1 id="something-important">Adopt Me!</h1>
-        <SearchParams />
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          {/* Renders the route that has highest score */}
+          {/* <SomeotherRoute path="/details/1" /> => this wins over :id*/}
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
