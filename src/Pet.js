255ea8d0fe8c98aa515088501a8010fb91d8c721
asucarlos <kuwahara.mee@gmail.com>
2019-09-29 12:46:21 -0400

    add hooks

--- src/Pet.js
+++ src/Pet.js
@@ -1,8 +1,11 @@
 import React from "react";
 export default function Pet({ name, animal, breed }) {
+  const x = name.toUpperCase(); //in {}, expression is allowed, statement not.
+  //in this case this whole x = name... is a statement while name.toUpperCase() is expression.
+  //ternary operators are expression so allowed
   return (
     <div>
-      <h1>{name}</h1>
+      <h1>{x}</h1>
       <h2>{animal}</h2>
       <h2>{breed}</h2>
     </div>
